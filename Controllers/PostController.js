7f3c7f1189aa438c5dcb1b842ffa60eff1c2da83
r4mim <r4mimu701@gmail.com>
2023-03-10 16:50:12 +0200

    - Added the getSomePosts function the function will return an array of objects, each object represents a post the posts are posts from users who the active user follows

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -1,6 +1,10 @@
 const db = require('../models');
 const Post = db.Post;
 const Following = db.Following;
+const User = db.User;
+const PostLike = db.PostLike;
+const Comment = db.Comment;
+const CommentLike = db.CommentLike;
 
 // The method for creating a new post
 exports.create = (req, res) => {
@@ -86,7 +90,6 @@ exports.getSomePosts = (req, res) => {
     const limit = 5;
 
     if(!req.session.auth){
-        console.log('i am here22222')
         res.redirect('/');
     }else{
         const user_id = req.session.user.user_id;
@@ -98,6 +101,7 @@ exports.getSomePosts = (req, res) => {
             for(let i = 0; i < rows.length; i++){
                 ids.push(rows[i].user_id); 
             }
+            // get some posts from each user that the active user if following
             const postPromises = ids.map(id => { // create a promise for each id
                 return Post.findAll({ // this query will be executed for each id 
                     where: {user_id: id},
@@ -106,16 +110,223 @@ exports.getSomePosts = (req, res) => {
             })
             Promise.all(postPromises) // to execute all queries
             .then(posts => {
+                let postsIds = [];
                 const flattenedPosts = [].concat(...posts);
-                const shuffledPosts = flattenedPosts.sort(() => Math.random() - 0.5); // shuffle the posts randomly
-                res.render('home', {posts: shuffledPosts});
+                for(let i = 0; i < flattenedPosts.length; i++){
+                    postsIds.push(flattenedPosts[i].post_id);
+                }
+                console.log(postsIds);
+                // for each post, get the ids of the users who liked this post
+                const likersIdsPromises = postsIds.map(id => {
+                    return PostLike.findAll({where: {post_id: id}})
+                })
+                
+                Promise.all(likersIdsPromises)
+                .then(likers => {
+                    let likersInfoPromises = []; 
+                    for(let i = 0; i < likers.length; i++){
+                        const likersInfoPromisesForPost = likers[i].map(like => {
+                            return User.findOne({where: {user_id: like.user_id}})
+                        });
+                        likersInfoPromises.push(Promise.all(likersInfoPromisesForPost));
+                    }
+                    Promise.all(likersInfoPromises) 
+                    .then(rows => {
+                        let likersInfo = [];
+                        for(let i = 0; i < rows.length; i++){
+                            const flattenedRows = [].concat(...rows[i]);
+                            let postLikersInfo = [];
+                            for(let j = 0; j < flattenedRows.length; j++){
+                                postLikersInfo.push({
+                                    likerId: flattenedRows[j].dataValues.user_id,
+                                    likerProfilePic: flattenedRows[j].dataValues.img_url,
+                                    likerUsername: flattenedRows[j].dataValues.username,
+                                })
+                            }
+                            likersInfo.push({
+                                postId: postsIds[i], // add postId to the object
+                                likers: postLikersInfo
+                            });
+                            
+                        }
+                        // res.send(likersInfo);
+                        // get the poster information and associate each poster information with his post
+                        const userPromises = flattenedPosts.map(post => {
+                            return User.findOne({where: {user_id: post.user_id}});
+                        });
+                        
+                        Promise.all(userPromises)
+                        .then(users => {
+                            const postInfo = [];
+                            for (let i = 0; i < flattenedPosts.length; i++) {
+                                const post = flattenedPosts[i];
+                                const user = users[i];
+                                postInfo.push({
+                                    post_id: post.post_id,
+                                    caption: post.caption,
+                                    img_url: post.img_url,
+                                    user_id: user.user_id,
+                                    profileImg: user.img_url,
+                                    username: user.username,
+                                });
+                            }
+                            // add each likers array in likersInfo to the right post object in the postInfo array
+                            for(let i = 0; i < likersInfo.length; i++){
+                                for(let j = 0; j < postInfo.length; j++){
+                                    if(likersInfo[i].postId == postInfo[j].post_id){
+                                        postInfo[j].likers = likersInfo[i].likers;
+                                    }
+                                }
+                            }
+                            // res.send(postInfo);
+                            // get all comments on the posts
+                            const commentsPromises = flattenedPosts.map(post => {
+                                return Comment.findAll({where: {post_id: post.post_id}});
+                            })
+                            Promise.all(commentsPromises)
+                            .then(result => {
+                                let comments = [];
+                                let singlePostComments = [];
+                                for(let i = 0; i < result.length; i++){
+                                    for(let j = 0; j < result[i].length; j++){
+                                        if(result[i]){
+                                            let comment = {
+                                                comment_id: result[i][j].comment_id,
+                                                commenter_id: result[i][j].user_id,
+                                                caption: result[i][j].caption,
+                                            }
+                                            singlePostComments.push(comment);
+                                        }
+                                    }
+                                    if(result[i][0]){
+                                        comments.push({
+                                            post_id: result[i][0].post_id,
+                                            comments: singlePostComments,
+                                        })
+                                        singlePostComments = [];  
+                                    }
+                                }
+                                // res.json(comments);
+                                // get the number of likes for each comment
+                                let commentsLikesPromises = []; 
+                                let commentsLikesCount = [];
+                                comments.forEach((post) => {
+                                    post.comments.forEach((comment) => {
+                                        commentsLikesPromises.push(CommentLike.findAll({where: {comment_id: comment.comment_id}}));
+                                    })
+                                })
+                                Promise.all(commentsLikesPromises)
+                                .then(result => {
+                                    result.forEach((comment) => {
+                                        if(comment.length >= 1){
+                                            commentsLikesCount.push({
+                                                comment_id: comment[0].comment_id,
+                                                likesCount: comment.length,
+                                            })
+                                        }
+                                    })
+                                    // res.send(commentsLikesCount);
+                                    // move all comments from the comments array to the main array (postInfo)
+                                    for(let i = 0; i < comments.length; i++){
+                                        for(let j = 0; j < postInfo.length; j++){
+                                            if(comments[i].post_id == postInfo[j].post_id){
+                                                postInfo[j].comments = comments[i].comments;
+                                            }
+                                        }
+                                    }
+                                
+                                    // move the comments likes count from commentsLikesCount array to the main array (postInfo)
+                                    for(let i = 0; i < commentsLikesCount.length; i++){
+                                        console.log('1')
+                                        postInfo.forEach(post => {
+                                            if(post.comments){
+                                                post.comments.forEach(comment => {
+                                                    if(commentsLikesCount[i].comment_id == comment.comment_id){
+                                                        comment.likesCount = commentsLikesCount[i].likesCount;
+                                                    }
+                                                })
+                                            }
+                                        })
+                                    }                 
+                                    // res.send(postInfo);
+
+                                    // get each commenter information (profile image and username)
+                                    let commenterInfoPromises = [];
+                                    postInfo.forEach(post => {
+                                        if(post.comments){
+                                            post.comments.forEach(comment => {
+                                                const promise = User.findOne({where: {user_id: comment.commenter_id}})
+                                                    .then(user => {
+                                                        return {
+                                                            commenterProfileImg: user.img_url,
+                                                            commenterUsername: user.username,
+                                                            comment_id: comment.comment_id,
+                                                        };
+                                                    });
+                                                commenterInfoPromises.push(promise);
+                                            })
+                                        }
+                                    })
+                                    // adding the each commenter information to the main array
+                                    Promise.all(commenterInfoPromises)
+                                    .then(result => {
+                                        result.forEach(commenterInfo => {
+                                            postInfo.forEach(post => {
+                                                if(post.comments){
+                                                    post.comments.forEach(comment => {
+                                                        if(commenterInfo.comment_id == comment.comment_id){
+                                                            comment.commenterProfileImg = commenterInfo.commenterProfileImg;
+                                                            comment.commenterUsername = commenterInfo.commenterUsername;
+                                                        }
+                                                    })
+                                                }
+                                            })
+                                        })
+                                        res.send(postInfo);
+                                    })
+                                    .catch(err => {
+                                        res.status(500).send({message: 'Error while getting commenters information; ' + err});
+                                    })
+                                })
+                                .catch(err => {
+                                    res.status(500).send({message: 'Error while getting comments likes: ' + err});
+                                })
+                                
+                            })
+                            .catch(err => {
+                                res.status(500).send({message: 'Error while getting comments: ' + err});
+                            })
+                        })
+                        .catch(err => {
+                            res.status(500).send({message: 'Error while getting users: ' + err});
+                        });
+                          
+                    })
+                    .catch(err => {
+                        res.status(500).send({message: 'Error while getting likers info: ' + err});
+                    })
+                })
+                .catch(err => {
+                    res.status(500).send({message: 'Error while getting likers: ' + err});
+                });
+                
+                
+                
+                
+                
+                // res.send(postsLikes);
+                // res.send(ids);
+                // res.render('home', {posts: shuffledPosts});
             })
             .catch(err => {
                 res.status(500).send({message: 'Error while getting posts: ' + err});
             })
+
+            
         })
         .catch(err => {
             res.status(500).send({message: 'Error while getting following: ' + err});
         })
     }
 }
+
